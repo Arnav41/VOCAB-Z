1befcae4f57d347ec5d2cf6c28b11beff8d50d35
Arnav41 <arnav04jan@gmail.com>
2023-02-09 20:46:42 +0530

    added a clear method

--- app.js
+++ app.js
@@ -2,17 +2,35 @@ const srcVal = document.getElementById("WordSrc")
 const newBtn = document.getElementById("New")
 const meaning = document.getElementById("meaning")
 const meaning2 = document.getElementById("meaning2")
+const ex = document.getElementById("ex")
+
+
 
 newBtn.addEventListener("click", () => {
   getWord(srcVal.value).then(data => {
+    //clearing after every click
+
+    meaning.innerText = "..."
+
+    meaning2.innerText = "..."
+    
+    ex.innerText = "..."
+
+    // uploading values
     let def = data[0].meanings[0].definitions[0].definition
 		meaning.innerText = def
+
+
     let def2 = data[0].meanings[0].definitions[1].definition
     meaning2.innerText = def2
 
 
+    let exm = data[0].meanings[1].definitions[0].example
+    ex.innerText = exm
+
   })
 })
+
 
 async function getWord(word) {
   const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
