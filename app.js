24feb4dbff64afa3d9f319688f5b2966d64d3b31
Arnav41 <arnav04jan@gmail.com>
2023-02-09 20:25:01 +0530

    added 2nd definition

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const srcVal = document.getElementById("WordSrc")
 const newBtn = document.getElementById("New")
 const meaning = document.getElementById("meaning")
+const meaning2 = document.getElementById("meaning2")
 
 newBtn.addEventListener("click", () => {
   getWord(srcVal.value).then(data => {
     let def = data[0].meanings[0].definitions[0].definition
-		console.log(def)
 		meaning.innerText = def
+    let def2 = data[0].meanings[0].definitions[1].definition
+    meaning2.innerText = def2
+
+    
   })
 })
 
